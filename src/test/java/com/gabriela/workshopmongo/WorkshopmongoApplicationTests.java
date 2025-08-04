5057156ec47b429fe71e8045f83b20e5074ea0d5
Gabriela <bela.vieira.pacheco@gmail.com>
2025-08-04 15:36:30 -0300

    Primeiro commit Projeto criado

---
+++ src/test/java/com/gabriela/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriela.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
